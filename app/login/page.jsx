13fc789b5819308c6cd2e22f7a51ff137aeb4b0f
daniel692a <daniel62armas@gmail.com>
2023-02-25 08:25:38 -0600

    Add login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,30 @@
+'use client';
+
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { MirrorWorld, ClusterEnvironment } from "@mirrorworld/web3.js"
+
+
 export default function Login(){
-    return(
-        <h1>
-            Login
-        </h1>
+    const router = useRouter();
+    const mirrorworld = new MirrorWorld({
+        apiKey: "mw_TxOBb8pfqKz3xmoJkvkZWyqHGZQRVsGP2tV", // Replace this with the API Key for your project
+        env: ClusterEnvironment.testnet, // Can be ClusterEnvionment.mainnet for mainnet
+    });
+
+    const [user, setUser] = useState()
+
+    async function login() {
+        const { user } = await mirrorworld.login()
+        router.push('/menu')
+        setUser(user)
+    }
+
+    return (
+        <div>
+            <main>
+                <button onClick={login}>Login to Mirror World</button>
+            </main>
+        </div>
     )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,10 @@
+import { MirrorWorld, ClusterEnvironment } from "@mirrorworld/web3.js"
+
 export default function HomePage() {
+    const mirrorworld = new MirrorWorld({
+        apiKey: "mw_TxOBb8pfqKz3xmoJkvkZWyqHGZQRVsGP2tV", // Replace this with the API Key for your project
+        env: ClusterEnvironment.testnet, // Can be ClusterEnvionment.mainnet for mainnet
+    });
     return (
         <h1>
             Welcome!
